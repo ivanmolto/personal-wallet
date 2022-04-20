6863ba1fd9418d43aaddc73d9b96c891938266c1
Ivan Molto <ivanmolto@gmail.com>
2022-04-20 08:31:51 +0200

    chore: Create CLI commands

---
+++ cmd/importWallet.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2022 Ivan Molto <ivanmolto@gmail.com>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// importWalletCmd represents the importWallet command
+var importWalletCmd = &cobra.Command{
+	Use:   "importWallet",
+	Short: "Imports and existing wallet",
+	Long: `Imports an existing wallet from a given private key.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("importWallet called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(importWalletCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// importWalletCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// importWalletCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
