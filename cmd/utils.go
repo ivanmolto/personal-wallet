72dda01b4becf701d662bee4e8fe1bb17b316dad
Ivan Molto <ivanmolto@gmail.com>
2022-04-20 08:46:06 +0200

    chore: Add utils

---
+++ cmd/utils.go
@@ -0,0 +1,18 @@
+package cmd
+
+import(
+	"context"
+	"io/ioutil"
+	"log"
+
+	"github.com/portto/solana-go-sdk/client"
+	"github.com/portto/solana-go-sdk/client/rpc"
+	"github.com/portto/solana-go-sdk/common"
+	"github.com/portto/solana-go-sdk/program/sysprog"
+	"github.com/portto/solana-go-sdk/types"
+)
+
+type Wallet struct{
+	account types.account
+	c *client.Client
+}
